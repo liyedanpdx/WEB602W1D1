98932ddc8b8621cb7cbc2cc80b14e558205ed623
Yedan Li <63386101+liyedanpdx@users.noreply.github.com>
2024-09-03 22:31:51 -0500

    second commit

--- node-example3.js
+++ node-example3.js
@@ -2,3 +2,17 @@ const rect = {
     perimeter: (x, y) => 2 * (x + y),
     area: (x, y) => x * y
 };
+function solveRect(l,w){
+    console.log(`Solving for rectangle with dimemsions: ${l}, ${w}`); // Length and Width
+    if (l<=0 || w <= 0){// if statement for l or w is less than zero
+        console.log(`Rectangle must be greater than zero. Value: ${l}, ${w}`);
+    } else {
+        console.log(`Area of rectangle: ${rect.area(l,w)}`);
+        console.log(`Perimeter of rectangle: ${rect.perimeter(l,w)}`);
+    }
+}
+
+solveRect(6,2);
+solveRect(8,1);
+solveRect(0,9); // Error
+solveRect(4,-1); // Error
